0d28343a52247c5f765ecb6864e24543b9c31f7d
mcdonam7 <mcdonam7@tcd.ie>
2018-10-01 11:26:40 +0100

    second commit

--- src/LCAmain.java
+++ src/LCAmain.java
@@ -1,7 +1,18 @@
 //Mary McDonald
 
-//15316382
-//Lowest Common Ancestor
-public class LCAmain {
+//15316382  
+//Lowest Common Ancestor - s.e
+import java.util.ArrayList; 
+import java.util.List;
 
-}
+
+public class LCAmain {
+	
+	
+	 public static void main(String[] args) 
+	    { 
+		 
+		 
+	    }
+	    }
+
